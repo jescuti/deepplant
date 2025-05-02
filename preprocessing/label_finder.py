2b337c0c0ad7a3d6b3f6bcc645ffe7eb306fec31
juliana <juliana_escuti@brown.edu>
2025-05-02 18:30:48 -0400

    feature: added label detection

--- preprocessing/label_finder.py
+++ preprocessing/label_finder.py
@@ -1,4 +1,40 @@
-import cv2; 
+import cv2 
+import torch
+import torchvision
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry, SamPredictor
+import supervision as sv
+
+def segment_labels_sam(img, mask_generator):
+    #mask finder
+    image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    print("SAM is generating masks!")
+    masks = mask_generator.generate(image_rgb)
+    print("SAM found "+str(len(masks))+" masks in this image")
+
+    mask_annotator = sv.MaskAnnotator(color_lookup=sv.ColorLookup.INDEX)
+    detections = sv.Detections.from_sam(sam_result=masks)
+    annotated_image = mask_annotator.annotate(img, detections)
+
+    cv2.imshow("Image", annotated_image)
+    cv2.waitKey(0)
+
+    labels = []
+    for mask in masks:
+        bbox = mask["bbox"]
+        x = bbox[0]
+        y = bbox[1]
+        w = bbox[2]
+        h = bbox[3]
+        #only return contours of a certain size
+        if mask["area"] > 30000 and mask["area"] < 50000:
+            cv2.putText(img, str(mask["area"]), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        labels.append(bbox)
+    
+    sv.plot_image(img)
+
+    cv2.destroyAllWindows()
+    return labels
 
 def segment_labels(img):
     # blur and threshold repeatedly to isolate label
@@ -12,7 +48,7 @@ def segment_labels(img):
     cv2.imshow("Image", thresh)
     cv2.waitKey(0)
 
-    blurred = cv2.GaussianBlur(thresh, (51, 51), 1000)
+    blurred = cv2.GaussianBlur(thresh, (51, 51), 10)
     cv2.imshow("Image", blurred)
     cv2.waitKey(0)
     ret, thresh2 = ret,thresh = cv2.threshold(blurred,230,255,0)
