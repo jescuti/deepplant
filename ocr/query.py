1a2b3cc47fdcf02e8df465621eee9ff4127f0418
Demi Le <linh_le_dieu@brown.edu>
2025-05-09 18:50:02 -0400

    clean up query code

--- ocr/query.py
+++ ocr/query.py
@@ -1,24 +1,15 @@
 import json
-# import pprint
+import re
+import time
+from fpdf import FPDF
 from ocr import run_clean_ocr
 from build_token_db import read_image_and_preprocess, DATABASE_FILENAME
 from search_db import search_phrase, search_list_of_phrases
-from fpdf import FPDF
-import re
 
 _BDR_CODES = re.compile(r'([0-9]{6})')
 OUTPUT_FILENAME = "output.pdf"
 
-def extract_bdr_code(list_of_paths: list[str]) -> list[str]:
-    list_of_codes : list[str] = []
-    for path in list_of_paths:
-        code = re.search(_BDR_CODES, path)
-        if code:
-            list_of_codes.append(code.group(0))
-    return list_of_codes
-
-
-def generate_pdf(list_of_codes: list[str], list_of_paths: list[str]) -> None:
+def generate_pdf(list_of_paths: list[str]) -> None:
     """
     Generates a PDF file that contains the cropped out labels and their corresponding image URLs.
 
@@ -39,7 +30,8 @@ def generate_pdf(list_of_codes: list[str], list_of_paths: list[str]) -> None:
     pdf.set_text_color(0, 0, 255)
     pdf.set_auto_page_break(True)
 
-    for (code, path) in zip(list_of_codes, list_of_paths):
+    for path in list_of_paths:
+        code = re.search(_BDR_CODES, path).group(0)
         pdf.cell(
             200, 5, txt=f"https://repository.library.brown.edu/iiif/image/bdr:{code}/full/full/0/default.jpg", \
                 ln=1, align="L", \
@@ -63,20 +55,12 @@ def query_by_label(text_label, database):
 
 def query_by_image(file_path: str, database: dict[str, list[str]], labeled: bool=True):
     """
-    Given an image, segment label (if needed), extract texts, and find images that match.
-    TODO: 
-    1. Add SAM for full picture
-    2. Input format of image?
+    Given an image of a label, extract texts, and find images that match.
     """
     # Read in input image
     img = read_image_and_preprocess(file_path)
     if img is None:
         raise FileNotFoundError(f"Could not load image at {file_path!r}")
-
-    # Run SAM on input image and get a list of labels
-    if not labeled:
-        pass
-        segmented_labels = []
 
     # Run OCR on recognized labels and get a list of phrases
     input_phrases = run_clean_ocr(img)
@@ -87,25 +71,27 @@ def query_by_image(file_path: str, database: dict[str, list[str]], labeled: bool
 
 
 def main():
-    # with open("databases/5k_db_1.json", "rb") as f:
-    with open(DATABASE_FILENAME, "rb") as f:
+    with open("databases/db_labels.json", "rb") as f:
+    # with open(DATABASE_FILENAME, "rb") as f:
         database = json.load(f)
     
     ### TEST QUERY BY LABEL
     text_label = "rocky mountain flora lat 3941"
+    logic_start = time.time()
     list_of_paths = query_by_label(text_label, database)
-    print(f"Finished querying. Number of images found: {len(list_of_paths)}.")
-    list_of_codes = extract_bdr_code(list_of_paths)
+    num_images = len(list_of_paths)
+    print(f"Finished querying. Number of images found: {num_images}.")
     print("Started generating PDF...")
-    # This step generally takes the longest
-    generate_pdf(list_of_codes, list_of_paths)
+    generate_pdf(list_of_paths)  # This step generally takes the longest
+    logic_end = time.time()
     print("Finished generating PDF!")
+    total_time = logic_end - logic_start
+    print(f"Average runtime over {num_images} images: {total_time/num_images:.5f} seconds")
 
     ### TEST QUERY BY IMAGE
     # image_path = "../image_download/db_labels/Mertensia alpina_bdr_754912.jpg"
     # list_of_paths = query_by_image(image_path, database)
-    # list_of_codes = extract_bdr_code(list_of_paths)
-    # generate_pdf(list_of_codes, list_of_paths)
+    # generate_pdf(list_of_paths)
 
 
 if __name__ == "__main__":
